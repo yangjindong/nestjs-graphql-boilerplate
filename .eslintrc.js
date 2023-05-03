7768313310dcda49932ad7a1e69b79e4bf25635f
Jindong Yang <yangjindong@gmail.com>
2023-05-03 20:38:37 +0800

    add needed packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,17 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'jest',
+    'prettier',
+    'import',
+    'unused-imports',
+  ],
   extends: [
     'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
     'plugin:prettier/recommended',
   ],
   root: true,
@@ -21,5 +29,35 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'no-unused-vars': 0,
+    '@typescript-eslint/no-unused-vars': 0,
+    'unused-imports/no-unused-imports': 1,
+    'unused-imports/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'none',
+        ignoreRestSiblings: true,
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        alphabetize: { order: 'asc', caseInsensitive: false },
+        'newlines-between': 'always-and-inside-groups',
+        warnOnUnassignedImports: true,
+      },
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          '**/*.test.{ts,js}',
+          '**/*.spec.{ts,js}',
+          './test/**.{ts,js}',
+          './scripts/**/*.{ts,js}',
+        ],
+      },
+    ],
   },
 };
